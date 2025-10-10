6dce816c5af3185338544ec80ec22b113c25f45a
tabrid <mollik15-5096@diu.edu.bd>
2025-10-11 02:34:34 +0600

    Add free order API with Purchases record creation, update payment methods

--- client/src/utils/baseurls.js
+++ client/src/utils/baseurls.js
@@ -1,6 +1,6 @@
 export const getBaseUrl = () => {
-  // return "https://api.bdmouza.com";
-  return "http://127.0.0.1:8000";
+  return "https://api.bdmouza.com";
+  // return "http://127.0.0.1:8000";
 };
 export const getBaseUrl1 = () => {
   return import.meta.env.VITE_API_BASE_URL2;
